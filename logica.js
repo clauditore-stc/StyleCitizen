e87c079b1d0ec50915ad7830693a774dd27a4ba2
clauditore-stc <claudioestevezmedina@gmail.com>
2026-08-01 11:44:39 +0100

    añadido funcionamiento de botones
    añadido el funcionamiento de los botones y el link para que conecten con la futura carpeta de imagenes y pueda mostrar cascos cuando le das al botón de cascos

--- logica.js
+++ logica.js
@@ -1,6 +1,13 @@
 // logica de StyleCitizen
 
+let cascos = [
+    { nombre: "Arden-SL", imagen: "img/cascos/arden-sl.png" },
+    { nombre: "Novikov", imagen: "img/cascos/novikov.png" }
+]
 
 document.getElementById("btn-casco").addEventListener("click", function() {
-  alert("Has seleccionado: Casco")
+    document.getElementById("lista-piezas").innerHTML = ""
+    for (let casco of cascos) {
+        document.getElementById("lista-piezas").innerHTML += `<img src="${casco.imagen}"/> <p>${casco.nombre}</p>`
+    }
 })
