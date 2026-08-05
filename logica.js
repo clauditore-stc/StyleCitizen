86b7e01ce39c950c9315dd7110462935cbb68da6
clauditore-stc <claudioestevezmedina@gmail.com>
2026-08-05 13:38:54 +0100

    añadido torso, brazos y piernas

--- logica.js
+++ logica.js
@@ -11,3 +11,42 @@ document.getElementById("btn-casco").addEventListener("click", function() {
         document.getElementById("lista-piezas").innerHTML += `<img src="${casco.imagen}"/> <p>${casco.nombre}</p>`
     }
 })
+
+
+let torso = [
+    { nombre: "Arden-SL", imagen: "img/torso/arden-sl.png" },
+    { nombre: "Novikov", imagen: "img/torso/novikov.png" }
+]
+
+document.getElementById("btn-torso").addEventListener("click", function() {
+    document.getElementById("lista-piezas").innerHTML = ""
+    for (let torso of torso) {
+        document.getElementById("lista-piezas").innerHTML += `<img src="${torso.imagen}"/> <p>${torso.nombre}</p>`
+    }
+})
+
+
+let brazos = [
+    { nombre: "Arden-SL", imagen: "img/brazos/arden-sl.png" },
+    { nombre: "Novikov", imagen: "img/brazos/novikov.png" }
+]
+
+document.getElementById("btn-brazos").addEventListener("click", function() {
+    document.getElementById("lista-piezas").innerHTML = ""
+    for (let brazos of brazos) {
+        document.getElementById("lista-piezas").innerHTML += `<img src="${brazos.imagen}"/> <p>${brazos.nombre}</p>`
+    }
+})
+
+
+let piernas = [
+    { nombre: "Arden-SL", imagen: "img/piernas/arden-sl.png" },
+    { nombre: "Novikov", imagen: "img/piernas/novikov.png" }
+]
+
+document.getElementById("btn-piernas").addEventListener("click", function() {
+    document.getElementById("lista-piezas").innerHTML = ""
+    for (let piernas of piernas) {
+        document.getElementById("lista-piezas").innerHTML += `<img src="${piernas.imagen}"/> <p>${piernas.nombre}</p>`
+    }
+})
